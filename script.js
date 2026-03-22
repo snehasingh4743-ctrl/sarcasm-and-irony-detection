aff8702f511acce711b9eadb0a15d3e9425c5aa0
snehasingh4743-ctrl <snehasingh4743@gmail.com>
2026-03-22 22:31:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sarcasm Detector</title>
-    <link rel="stylesheet" href="/static/style.css">
-</head>
-<body>
+function predict() {
+    let text = document.getElementById("text").value;
 
-<div class="container">
-    <h1>Sarcasm Detector</h1>
-
-    <textarea id="text" placeholder="Enter text here..."></textarea><br>
-
-    <button onclick="predict()">Check</button>
-
-    <h2 id="output"></h2>
-</div>
-
-<script src="/static/script.js"></script>
-</body>
-</html>
+    fetch("/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text: text })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("output").innerText = data.result;
+    });
+}
